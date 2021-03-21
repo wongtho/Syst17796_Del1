a6906674b53b08cc33a936163f23597dbef2ee26
wongtho <78035657+wongtho@users.noreply.github.com>
2021-03-21 13:16:56 -0300

    Added base starter code

---
+++ src/highvslow/HighvsLow.java
@@ -0,0 +1,19 @@
+/**
+ *  @author Thomas Wong, 991623854
+ *  Campus: Davis
+ *  Course: Syst17796 Fundamentals of Software Design and Development
+ *  Program: Deliverable 1 Driver Method
+ *  Due Date: March 21, 2021
+ */
+package highvslow;
+
+public class HighvsLow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
