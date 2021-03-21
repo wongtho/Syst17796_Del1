3359550caad01e436818d79abe53fba5255961fe
Thomas Wong <wongtho@sheridancollege.ca>
2021-03-21 14:49:44 -0300

    Created PlayerHighLow class inherit from Player class

---
+++ src/highvslow/PlayerHighLow.java
@@ -0,0 +1,23 @@
+/**
+ *  @author Thomas Wong, 991623854
+ *  Campus: Davis
+ *  Course: Syst17796 Fundamentals of Software Design and Development
+ *  Program: Deliverable 1 Game Class
+ *  Due Date: March 21, 2021
+ */
+package highvslow;
+
+//inherit from Game class
+
+public class PlayerHighLow extends Player {
+    
+    //conructor
+    public PlayerHighLow (String name) {
+        super(name);
+    }
+    @Override
+    public void play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
