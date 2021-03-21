02043a1234cdb68a2ec03249e2b01c356d2a3a3a
Thomas Wong <wongtho@sheridancollege.ca>
2021-03-21 19:15:27 -0300

    Added game instructions

--- src/highvslow/HighvsLow.java
+++ src/highvslow/HighvsLow.java
@@ -34,7 +34,16 @@ public class HighvsLow {
         //title
         System.out.println("Title of this game is: " + HighvsLow.getGameName());
         //include instructions
-        
+        System.out.println("Here are the game instructions: ");
+        System.out.println("1. Each player is dealt 5 cards from a shuffled deck of standard 52 cards");
+        System.out.println("2. The remaining deck of cards is placed in the middle.");
+        System.out.println("3. For every round, 1 card is dealt from the center deck of cards into the round pile.");
+        System.out.println("4. Each player must then deal any 1 card from their hand into the round pile.");
+        System.out.println("5. The player with the highest weighted card that is also higher than the inital dealt card into the round pile wins 1 point.");
+        System.out.println("6. Each player draws 2 cards from the middle deck of cards.");
+        System.out.println("7. Steps 3 to 6 is repeated until no more cards can be drawn from Step 6.");
+        System.out.println("*. The player with the highest total points wins");
+
         //players
         for(int i = 0; i < HighvsLow.getPlayers().size(); i++) {
             System.out.println("Player " + (i + 1) + " is: " + HighvsLow.getPlayers().get(i).getPlayerID());
