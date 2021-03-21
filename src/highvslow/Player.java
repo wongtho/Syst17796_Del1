d22b3ba5bb5403cfb0b661a6b24397a37bcb2702
Thomas Wong <wongtho@sheridancollege.ca>
2021-03-21 13:48:49 -0300

    Added comments and formatting

--- src/highvslow/Player.java
+++ src/highvslow/Player.java
@@ -5,35 +5,27 @@
  *  Program: Deliverable 1 Game Class
  *  Due Date: March 21, 2021
  */
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
+
 package highvslow;
 
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
- * @author dancye, 2018
  */
-public abstract class Player 
-{
+public abstract class Player {
     private String playerID; //the unique ID for this player
     
     /**
      * A constructor that allows you to set the player's unique ID
      * @param name the unique ID to assign to this player.
      */
-    public Player(String name)
-    {
+    public Player(String name) {
         playerID= name;
     }
     
     /**
      * @return the playerID
      */
-    public String getPlayerID() 
-    {
+    public String getPlayerID() {
         return playerID;
     }
 
@@ -41,8 +33,7 @@ public abstract class Player
      * Ensure that the playerID is unique
      * @param givenID the playerID to set
      */
-    public void setPlayerID(String givenID) 
-    {
+    public void setPlayerID(String givenID) {
         playerID = givenID;
     }
     
