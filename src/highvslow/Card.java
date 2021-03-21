3e35bf6d20d98763a1c95973ba4cc59578736fea
Thomas Wong <wongtho@sheridancollege.ca>
2021-03-21 13:39:23 -0300

    Models card from standard deck of 52 cards

--- src/highvslow/Card.java
+++ src/highvslow/Card.java
@@ -5,27 +5,31 @@
  *  Program: Deliverable 1 Card Class
  *  Due Date: March 21, 2021
  */
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
-package highvslow;
 
+package highvslow;
 /**
- * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
- * @author dancye, 2018
+ * This class models 1 single card of a standard deck of 52 cards
+ * @author Thomas Wong
  */
 public abstract class Card {
-    //default modifier for child classes
-    
-    /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    
+        //create Standard deck of 52 card properties
+        //implemented from ICE 1 and 2
+        public enum Suit {CLUBS, DIAMONDS, HEARTS, SPADE};
+        public enum Value{ACE, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, JACK, QUEEN, KING};
+        private final Suit suit;
+        private final Value value;
+        //constructor method
+        public Card(Suit s, Value gVal) {
+           suit = s;
+           value = gVal;
+        }
+        //getter methods
+	public Value getValue() {
+		return this.value;
+	}
+	public Suit getSuit() {
+		return this.suit;
+        }
     @Override
     public abstract String toString();
     
